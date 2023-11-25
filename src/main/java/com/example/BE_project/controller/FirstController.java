dca6d0f79c04c37d212fb867916a26d2a8be3aff
변지환 - Byeon JiHwan <99013865+Quswlghks@users.noreply.github.com>
2023-11-25 09:36:02 +0900

    Initial commit

---
+++ src/main/java/com/example/BE_project/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.BE_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        //모델 객체가 "지환"값을 "username"에 연결해 웹브라우저로 보냄
+        model.addAttribute("username", "지환");
+        return "greetings";
+    }
+}
---
+++ src/test/java/com/example/BE_project/BeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
